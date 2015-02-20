8b6419596dd2147f68d9379d75d982527a94853f
iroth <israel.roth@gmail.com>
2015-02-20 17:50:37 +0200

    few files missing from commit

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,5 +1,69 @@
 angular.module('starter.controllers', [])
 
+.controller('SignInCtrl', function($scope, $state) {
+
+  $scope.logout = function () {
+    openFB.revokePermissions(
+        function() {
+            alert('Permissions revoked');
+        },
+        function(error) {
+            alert(error.message);
+        }
+    );
+
+    openFB.fbLogout(
+        function() {
+            alert ('Facebook Logout Successful.');
+        },
+        function(error) {
+            alert(error.message);
+        }
+    );
+  }
+
+    $scope.fbLogin = function() {
+        openFB.login(
+            function(response) {
+                if (response.status === 'connected') {
+                    console.log('Got Token: ' + response.authResponse.token);
+                    var msgdata = {
+                            'token' : response.authResponse.token
+                        };
+                    $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+                    $http({
+                        method: 'POST',
+                        url: 'http://localhost:3000/api/connect',
+                        data: 'token='+response.authResponse.token
+                    })
+                    .success(function(data, status, headers, config) {
+                        console.log(data);
+                        console.log('call to our server works');
+                    })
+                    .error(function(data, status, headers, config) {
+                        console.log(data);
+                        console.log('call to our server fails');
+                    });
+                    $state.go('tabs.dash');
+                } else {
+                    alert('Facebook login failed');
+                    $state.go('tabs.home');
+                }
+            },
+            {scope: 'public_profile, user_friends'});
+    }
+
+  $scope.signIn = function() {
+    console.log('Sign-In');
+    $state.go('tabs.home');
+  };
+
+})
+
+.controller('HomeTabCtrl', function($scope) {
+  console.log('HomeTabCtrl');
+})
+
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
