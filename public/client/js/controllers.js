62ad3d6d94475e9f3ce21059c081c2be1076d0e9
David Virtser <dvirtser@google.com>
2015-02-21 18:42:13 +0200

    client changes

--- public/client/js/controllers.js
+++ public/client/js/controllers.js
@@ -88,11 +88,16 @@ angular.module('starter.controllers', ['ngStorage'])
 .controller('ResultsFriendsCtrl', function($scope,Results,LocalResults,Parties) {
   console.log('ResultsFriendsCtrl');
   $scope.parties = Parties.query(function(){
+    console.log($scope.parties);
     $scope.results = Results.query(function(){
       var total_number_of_votes = 0;
       angular.forEach($scope.results, function(value, key) {
         total_number_of_votes += value.number_of_votes;
-        value.name = $scope.parties[value.party_id].name;
+        angular.forEach($scope.parties, function(party, index) {
+          if (value.party_id == party.id)
+            value.name = party.name;
+        })
+        
         console.log(value);
       });
       $scope.results.total_number_of_votes = total_number_of_votes;
