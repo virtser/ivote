22c826e9b822cf96043a67a24cf988812f822fb9
David Virtser <dvirtser@google.com>
2015-02-21 15:08:38 +0200

    client changes

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,3 @@ cd ..
 cp -rf client/www/ public/client/
 git add --all .
 git commit -m "client changes"
-
-BRANCH=git rev-parse --abbrev-ref HEAD
-
-git push heroku $BRANCH:master
-heroku run rake db:migrate
-heroku run rake db:seed
