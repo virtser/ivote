fe33540d1141d385ca7c30284db3e0c5ef6cb6be
David Virtser <dvirtser@google.com>
2015-02-28 18:40:42 +0200

    client changes

--- build_client.sh
+++ build_client.sh
@@ -1,7 +1,9 @@
 cd client
 gulp
+gulp add-proxy
 cd ..
 rsync -av --delete --exclude='index.html' client/www/ public/client/
+gulp remove-proxy
 git add --all .
 git commit -m "client changes"
 # git push
