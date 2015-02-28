6931eb9008833e19f0591ce36e49ce0908c18b81
David Virtser <dvirtser@google.com>
2015-02-28 18:44:35 +0200

    api build fix

--- build_client.sh
+++ build_client.sh
@@ -1,9 +1,11 @@
 cd client
 gulp
-gulp add-proxy
+gulp remove-proxy
 cd ..
 rsync -av --delete --exclude='index.html' client/www/ public/client/
-gulp remove-proxy
+cd client
+gulp add-proxy
+cd..
 git add --all .
 git commit -m "client changes"
 # git push
