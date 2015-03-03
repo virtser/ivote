459784a781a2d0dbb60dfee1a292672b5475a2e5
David Virtser <dvirtser@google.com>
2015-03-03 23:27:07 +0200

    client changes

--- public/client/js/controllers.js
+++ public/client/js/controllers.js
@@ -145,7 +145,9 @@ angular.module('starter.controllers', ['ngStorage', 'ngCookies', 'ngCordova', 's
 .controller('ResultsFriendsCtrl', function($scope, $cordovaSocialSharing, Results, Parties) {
   
   $scope.renderImgSrc = function (id) {
-    console.log("renderImgSrc", id);
+    return '../img/parties/' + (id+1)+"-1.png";
+    
+   
   };
 
   $scope.parties = Parties.query(function(){
@@ -159,13 +161,14 @@ angular.module('starter.controllers', ['ngStorage', 'ngCookies', 'ngCordova', 's
           if (value.party_id == party.id)
             value.name = party.name;
         })
+        
         // console.log(value);
        
      });
 
-
       $scope.toggleItem = function (result) {
         var seats = result.number_of_votes / $scope.results.total_number_of_votes * 120;
+
         result.selected = !result.selected;
         $scope.totalSelected += seats * (result.selected ? 1 : -1);
         $scope.selectedPercents = $scope.totalSelected * 100 / 120;
