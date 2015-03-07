f645a52c41e3f9a785825c2ab27370248b5f6dc9
iroth <israel.roth@gmail.com>
2015-03-07 09:21:21 +0200

    update version and build script

--- client/build_android.sh
+++ client/build_android.sh
@@ -3,3 +3,4 @@ gulp remove-proxy
 ionic build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/ant-build/CordovaApp-release-unsigned.apk alias_name
 zipalign -f -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk platforms/android/ant-build/CordovaApp-release-aligned.apk
+gulp add-proxy
