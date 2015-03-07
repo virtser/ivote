6abaaa3d5cef2eefa123314735305b8f20da00d1
David Virtser <poison77@gmail.com>
2015-03-07 12:43:32 +0200

    implemented movile push

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,3 @@
-require 'mandrill'
 require 'mixpanel-ruby'
 
 class VotesController < ApplicationController
@@ -53,7 +52,7 @@ class VotesController < ApplicationController
         tracker = Mixpanel::Tracker.new('5169a311c1cad013734458bb88005dcd')
         tracker.track(vote_params[:user_id], 'Vote')
 
-        send_mail
+        Generic.send_notificatioin(params[:vote][:user_id], 'חבר/ה שלך הצביע/ה באפליקציית iVote.')
 
         format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
         format.json { render :show, status: :created, location: @vote }
@@ -73,7 +72,7 @@ class VotesController < ApplicationController
         tracker = Mixpanel::Tracker.new('5169a311c1cad013734458bb88005dcd')
         tracker.track(vote_params[:user_id], 'Vote Update')
 
-        send_mail
+        Generic.send_notificatioin(params[:vote][:user_id], 'חבר/ה שלך הצביע/ה באפליקציית iVote.')
         
         format.html { redirect_to @vote, notice: 'Vote was successfully updated.' }
         format.json { render :show, status: :ok, location: @vote }
@@ -95,56 +94,4 @@ class VotesController < ApplicationController
       params.require(:vote).permit(:user_id, :party_id)
     end
 
-    def send_mail
-      user_id = params[:vote][:user_id].to_s
-
-      friends = Relation.where(user_id: user_id).pluck(:friend_user_id)
-      emails = User.where("id IN (?)", friends).pluck(:email)
-
-      emailList = []
-      emails.each do |email|
-        unless email.nil?
-          emailList.push({"email"=>email, "type"=>"to"})
-        end
-      end
-
-      begin
-          mandrill = Mandrill::API.new '_jNnzxqtlL9rUB8Y7Kbhog'
-          template_name = "vote"
-          template_content = [{"content"=>"example content", "name"=>"example name"}]
-          message = {"subject"=>"iVote חבר/ה שלך הצביע/ה באפליקציית",
-           "text"=>"iVote חבר/ה שלך הצביע/ה באפליקציית",
-           "auto_html"=>nil,
-           "google_analytics_domains"=>["ivote.org.il"],
-           "tags"=>["friend-vote"],
-           "headers"=>{"Reply-To"=>"we@ivote.org.il"},
-           "return_path_domain"=>nil,
-           "auto_text"=>nil,
-           "to"=> emailList,
-           "from_name"=>"iVote App",
-           "preserve_recipients"=>nil,
-           "track_clicks"=>nil,
-           "track_opens"=>nil,
-           "inline_css"=>nil,
-           "from_email"=>"we@ivote.org.il",
-           "view_content_link"=>nil,
-           "url_strip_qs"=>nil,
-           "signing_domain"=>nil,
-           "tracking_domain"=>nil,
-           "important"=>false,
-           "html"=>""}
-          async = true
-
-          # ip_pool = "Main Pool"
-          # send_at = "example send_at"
-          result = mandrill.messages.send_template template_name, template_content, message, async
-
-          logger.info "Email sending result: " + result.to_yaml
-          
-      rescue Mandrill::Error => e
-          # Mandrill errors are thrown as exceptions
-          logger.error "A mandrill error occurred: #{e.class} - #{e.message}"
-          # raise
-      end   
-    end
 end
